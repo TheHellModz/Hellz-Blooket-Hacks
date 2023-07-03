86f332877ce8904c3a2d85fcdeb6547b3c79f539
TheHellModz <130288662+TheHellModz@users.noreply.github.com>
2023-07-03 19:33:42 +0100

    CryptoHack.js

--- CryptoHack.js
+++ CryptoHack.js
@@ -1,3 +1,13 @@
+/**
+* @README
+* Copyright (c) Hell Modz 2023 
+*
+* You may not reproduce or distribute any code inside this file without the licenser's permission.
+* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+* You may not under any circumstance republish any code from this file as your own.
+*/
+
+
 (async () => {
     let n = document.createElement('iframe');
     document.body.append(n);
