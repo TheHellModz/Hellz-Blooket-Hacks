dbb05c40534cfcb0558ff1090f90c2b8a7261258
TheHellModz <130288662+TheHellModz@users.noreply.github.com>
2023-07-03 20:23:23 +0100

    Always Tripple.js

--- Crypto Hacking Stuff/Always Tripple.js
+++ Crypto Hacking Stuff/Always Tripple.js
@@ -6,3 +6,8 @@
 * You may not copy, modify, steal, skid, or recreate any of the code inside this file.
 * You may not under any circumstance republish any code from this file as your own.
 */
+/* AINTG WORKING RN
+*/
+(async () => {
+      reactHandler().stateNode.setState({ choices: [{ type: "mult", val: 3, rate: .075, blook: "Brainy Bot", text: "Triple Crypto" }] }), 25);
+})();
